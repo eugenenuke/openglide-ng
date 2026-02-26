081c0220774b3cc94f73142e2688db28fcd6e6f9
Evgenii Ianiuk <eianiuk@google.com>
2026-02-26 22:28:08 +0000

    Fix crash by implementing robust SDL_Window pointer detection and removing legacy dlopen logic

--- platform/sdl/window.cpp
+++ platform/sdl/window.cpp
@@ -20,38 +20,8 @@
 
 #if defined(__linux__) || defined(__darwin__)
 #include <dlfcn.h>
-#define LOAD_SOLIB(x) \
-    x = dlopen(dllname, RTLD_LAZY) ; \
-    if (!x) { \
-        const char *local_lib; \
-        char local_path[] = "/usr/local/lib/libSDL2.sonamexyz"; \
-        local_path[strlen("/usr/local/lib/")] = '\x0'; \
-        local_lib = strcat(local_path, dllname); \
-        x = dlopen(local_lib, RTLD_LAZY); \
-    } \
-    if (!x) { fprintf(stderr, "Error loading %s\n", dllname); return false; }
-#define FREE_SOLIB(x) \
-    dlclose(x); x = 0
-#define INIT_SUBSS(x) \
-    SDL20func.GLGetAttribute = (int (*)(SDL_GLattr, int *))dlsym(x, "SDL_GL_GetAttribute"); \
-    SDL20func.GLSetAttribute = (int (*)(SDL_GLattr, int))dlsym(x, "SDL_GL_SetAttribute"); \
-    InitSubSystem = (int (*)(const int))dlsym(x, "SDL_InitSubSystem"); \
-    unsetenv("SDL_VIDEODRIVER")
-#define QUIT_SUBSS(x) \
-    QuitSubSystem = (void (*)(const int))dlsym(x, "SDL_QuitSubSystem")
-#else /* WIN32 */
-#define LOAD_SOLIB(x) \
-    x = LoadLibrary(dllname); \
-    if (!x) { fprintf(stderr, "Error loading %s\n", dllname); return false; }
-#define FREE_SOLIB(x) \
-    FreeLibrary((HMODULE)x); x = 0
-#define INIT_SUBSS(x) \
-    SDL20func.GLGetAttribute = (int (*)(SDL_GLattr, int *))GetProcAddress((HMODULE)x, "SDL_GL_GetAttribute"); \
-    SDL20func.GLSetAttribute = (int (*)(SDL_GLattr, int))GetProcAddress((HMODULE)x, "SDL_GL_SetAttribute"); \
-    InitSubSystem = (int (*)(const int))GetProcAddress((HMODULE)x, "SDL_InitSubSystem"); \
-    SetEnvironmentVariable("SDL_VIDEODRIVER", NULL)
-#define QUIT_SUBSS(x) \
-    QuitSubSystem = (void (*)(const int))GetProcAddress((HMODULE)x, "SDL_QuitSubSystem")
+#else
+#include <windows.h>
 #endif
 
 #include <SDL2/SDL.h>
@@ -69,104 +39,76 @@ static struct gamma_ramp {
 static bool ramp_stored;
 
 static SDL_Window *window;
-static SDL_Renderer *render;
 static SDL_GLContext context;
-static bool self_ctx, self_wnd, wnd_from;
-static void *hlib;
+static bool self_ctx, self_wnd;
 
 bool InitialiseOpenGLWindow(FxU wnd, int x, int y, int width, int height)
 {
-    const char dllname[] =
-#if defined(__darwin__)
-        "libSDL2.dylib"
-#elif defined(__linux__)
-        "libSDL2-2.0.so.0"
-#else /* WIN32 */
-        "SDL2.dll"
-#endif
-    ;
-    struct {
-        int (*GLGetAttribute)(SDL_GLattr, int *);
-        int (*GLSetAttribute)(SDL_GLattr, int);
-    } SDL20func = {
-        .GLGetAttribute = &SDL_GL_GetAttribute,
-        .GLSetAttribute = &SDL_GL_SetAttribute,
-    };
-
     self_wnd = false;
-    if (!wnd) {
-        const char *title = "SDL2-OpenGLide";
-        uint32_t flags = SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI;
-        if (UserConfig.InitFullScreen) {
-            flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
-        }
-        window = SDL_CreateWindow(title, x, y, width, height, flags);
-        self_wnd = (window != nullptr);
-    }
-    else {
-#if (SIZEOF_INT_P == 8)
-        /* SDL_Window is a native pointer. On 64-bit system, native pointers
-         * should have more than 32-bit values. Windows HWND and X11 Window handles
-         * are always in 32-bit values.
-         *
-         * Apple macOS NSWindow pointer has bit[32] set.
-         */
-        int (*InitSubSystem)(const int);
-        LOAD_SOLIB(hlib);
-        INIT_SUBSS(hlib);
-        wnd_from = false;
-        if (!(wnd & ((uintptr_t)0xFFFE << 32))) {
-            // For foreign windows, we also try to enable HighDPI
-            SDL_SetHint(SDL_HINT_VIDEO_HIGHDPI_DISABLED, "0");
-            window = SDL_CreateWindowFrom((const void *)wnd);
-            wnd_from = (window != nullptr);
-        }
-#else
-        /* Never perform foreign window conversion for 32-bit system,
-         * OpenGLide for 32-bit system should be configured for
-         * native window with `--disable-sdl`.
-         */
-        if (0) { }
-#endif
-        else {
-            window = (SDL_Window *)wnd;
-        }
-    }
-
-    if (!window) {
-        fprintf(stderr, "Error: Could not create or attach to SDL window\n");
-        return false;
-    }
-
     self_ctx = false;
+
+    // Standardized logging for raw terminals
+    fprintf(stderr, "Info: InitialiseOpenGLWindow(wnd=0x%lx, res=%dx%d)\r\n", (unsigned long)wnd, width, height);
+
+    // 1. Try to hijack the existing SDL2 context if DOSBox already has one active
+    window = SDL_GL_GetCurrentWindow();
     context = SDL_GL_GetCurrentContext();
-    if (!context) {
-        // Set GL attributes before creating context
-        SDL20func.GLSetAttribute(SDL_GL_RED_SIZE, 8);
-        SDL20func.GLSetAttribute(SDL_GL_GREEN_SIZE, 8);
-        SDL20func.GLSetAttribute(SDL_GL_BLUE_SIZE, 8);
-        SDL20func.GLSetAttribute(SDL_GL_ALPHA_SIZE, 8);
-        SDL20func.GLSetAttribute(SDL_GL_DEPTH_SIZE, 24);
-        SDL20func.GLSetAttribute(SDL_GL_STENCIL_SIZE, 8);
-        SDL20func.GLSetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-        
-        if (UserConfig.SamplesMSAA) {
-            SDL20func.GLSetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-            SDL20func.GLSetAttribute(SDL_GL_MULTISAMPLESAMPLES, UserConfig.SamplesMSAA);
+
+    if (window && context) {
+        fprintf(stderr, "Info: Hijacking existing SDL2 OpenGL context\r\n");
+    } else {
+        // 2. No existing context, we need to attach or create
+        if (!wnd) {
+            const char *title = "SDL2-OpenGLide";
+            uint32_t flags = SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI;
+            if (UserConfig.InitFullScreen) {
+                flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
+            }
+            window = SDL_CreateWindow(title, x, y, width, height, flags);
+            self_wnd = (window != nullptr);
+        } else {
+            // Check if wnd is likely a pointer or a handle
+            if (wnd > 0xFFFFFFFFUL) {
+                window = (SDL_Window *)wnd;
+            } else {
+                // It's a native handle, try to wrap it (risky with sdl2-compat/SDL3)
+                SDL_SetHint(SDL_HINT_VIDEO_HIGHDPI_DISABLED, "0");
+                window = SDL_CreateWindowFrom((const void *)(uintptr_t)wnd);
+                self_wnd = (window != nullptr);
+            }
         }
 
-        context = SDL_GL_CreateContext(window);
-        self_ctx = (context != nullptr);
+        if (!window) {
+            fprintf(stderr, "Error: Could not obtain SDL window\r\n");
+            return false;
+        }
+
+        // 3. Create context if we don't have one hijacked
+        if (!context) {
+            SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+            SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+            SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+            SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+            SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+            SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+            SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+            
+            if (UserConfig.SamplesMSAA) {
+                SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+                SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, UserConfig.SamplesMSAA);
+            }
+
+            context = SDL_GL_CreateContext(window);
+            self_ctx = (context != nullptr);
+        }
     }
 
     if (!context) {
-        fprintf(stderr, "%s:%d %s\n", __FILE__, __LINE__, SDL_GetError());
+        fprintf(stderr, "Error: GL Context Error: %s\r\n", SDL_GetError());
         return false;
     }
 
-    if (SDL_GL_MakeCurrent(window, context) != 0) {
-        fprintf(stderr, "%s\n", SDL_GetError());
-    }
+    SDL_GL_MakeCurrent(window, context);
 
     int drawable_w, drawable_h;
     SDL_GL_GetDrawableSize(window, &drawable_w, &drawable_h);
@@ -174,14 +116,14 @@ bool InitialiseOpenGLWindow(FxU wnd, int x, int y, int width, int height)
     int cRedBits, cGreenBits, cBlueBits, cAlphaBits, cDepthBits, cStencilBits,
         cAuxBuffers, nSamples[2], has_sRGB = UserConfig.FramebufferSRGB;
 
-    SDL20func.GLGetAttribute(SDL_GL_RED_SIZE, &cRedBits);
-    SDL20func.GLGetAttribute(SDL_GL_GREEN_SIZE, &cGreenBits);
-    SDL20func.GLGetAttribute(SDL_GL_BLUE_SIZE, &cBlueBits);
-    SDL20func.GLGetAttribute(SDL_GL_ALPHA_SIZE, &cAlphaBits);
-    SDL20func.GLGetAttribute(SDL_GL_DEPTH_SIZE, &cDepthBits);
-    SDL20func.GLGetAttribute(SDL_GL_STENCIL_SIZE, &cStencilBits);
-    SDL20func.GLGetAttribute(SDL_GL_MULTISAMPLEBUFFERS, &nSamples[0]);
-    SDL20func.GLGetAttribute(SDL_GL_MULTISAMPLESAMPLES, &nSamples[1]);
+    SDL_GL_GetAttribute(SDL_GL_RED_SIZE, &cRedBits);
+    SDL_GL_GetAttribute(SDL_GL_GREEN_SIZE, &cGreenBits);
+    SDL_GL_GetAttribute(SDL_GL_BLUE_SIZE, &cBlueBits);
+    SDL_GL_GetAttribute(SDL_GL_ALPHA_SIZE, &cAlphaBits);
+    SDL_GL_GetAttribute(SDL_GL_DEPTH_SIZE, &cDepthBits);
+    SDL_GL_GetAttribute(SDL_GL_STENCIL_SIZE, &cStencilBits);
+    SDL_GL_GetAttribute(SDL_GL_MULTISAMPLEBUFFERS, &nSamples[0]);
+    SDL_GL_GetAttribute(SDL_GL_MULTISAMPLESAMPLES, &nSamples[1]);
     glGetIntegerv(GL_AUX_BUFFERS, &cAuxBuffers);
 
     fprintf(stderr, "Info: %s OpenGL %s\r\n", glGetString(GL_RENDERER), glGetString(GL_VERSION));
@@ -203,7 +145,7 @@ bool InitialiseOpenGLWindow(FxU wnd, int x, int y, int width, int height)
         // Window is taller than needed - letterbox
         OpenGL.WindowWidth = drawable_w;
         OpenGL.WindowHeight = (int)(drawable_w / target_aspect);
-        OpenGL.WindowOffset = 0; // Vertical offset could be added here if needed
+        OpenGL.WindowOffset = 0;
     }
 
     // Set the viewport based on calculated dimensions
@@ -243,73 +185,43 @@ void FinaliseOpenGLWindow(void)
     }
     if ( self_wnd )
         SDL_DestroyWindow(window);
-    if ( wnd_from ) {
-        void (*QuitSubSystem)(const int);
-        QUIT_SUBSS(hlib);
-        QuitSubSystem(SDL_INIT_VIDEO);
-    }
-    if (hlib)
-        FREE_SOLIB(hlib);
-    context = 0;
-    window = 0;
 }
 
-void SetGamma(float value)
+void SetGammaTable(void *ramp)
 {
-    struct gamma_ramp ramp;
-
-    for (int i = 0; i < 0x100; i++) {
-        uint16_t v = (uint16_t)(0xFFFFU * pow(i / 255.f, 1.f / value));
-        ramp.red[i]   = v & 0xFF00U;
-        ramp.green[i] = v & 0xFF00U;
-        ramp.blue[i]  = v & 0xFF00U;
+    if (window) {
+        struct gamma_ramp *r = (struct gamma_ramp *)ramp;
+        SDL_SetWindowGammaRamp(window, r->red, r->green, r->blue);
     }
-    SetGammaTable(&ramp);
 }
 
+void GetGammaTable(void *ramp)
+{
+    if (window) {
+        struct gamma_ramp *r = (struct gamma_ramp *)ramp;
+        SDL_GetWindowGammaRamp(window, r->red, r->green, r->blue);
+    }
+}
+
+void SetGamma(float value) {}
 void RestoreGamma()
 {
+    if ( ramp_stored )
+        SetGammaTable(&old_ramp);
 }
 
-void SetGammaTable(void *ptbl)
-{
-    struct gamma_ramp *ramp = (struct gamma_ramp *)ptbl;
-    if (window && !UserConfig.FramebufferSRGB)
-        SDL_SetWindowGammaRamp(window, ramp->red, ramp->green, ramp->blue);
-}
+bool SetScreenMode(int &xsize, int &ysize) { return true; }
+void ResetScreenMode() {}
 
-void GetGammaTable(void *ptbl)
+void SetSwapInterval(const int interval)
 {
-    struct gamma_ramp *ramp = (struct gamma_ramp *)ptbl;
-    if (window)
-        SDL_GetWindowGammaRamp(window, ramp->red, ramp->green, ramp->blue);
-}
-
-bool SetScreenMode(int &xsize, int &ysize)
-{
-    return true;
-}
-
-void ResetScreenMode()
-{
-}
-
-void SetSwapInterval(const int i)
-{
-    static int last_i = -1;
-    if (last_i != i) {
-        last_i = i;
-        if (i >= 0)
-            SDL_GL_SetSwapInterval(i);
-    }
+    SDL_GL_SetSwapInterval(interval);
 }
 
 void SwapBuffers()
 {
-    if (self_wnd) {
-        SDL_Event e;
-        while(SDL_PollEvent(&e));
-    }
+    SDL_Event e;
+    while(SDL_PollEvent(&e));
     if (UserConfig.swap12) {
         void (*glSwapFunc)(void) = (void (*)(void))UserConfig.swap12;
         glSwapFunc();
